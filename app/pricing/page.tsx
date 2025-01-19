89b7370963fddec9d1664d0cac0978df47b944ff
Haylee Ulrey <haylee@rogueworkforce.org>
2025-01-19 13:09:15 -0800

    Update website design and content

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,14 +1,13 @@
 import PricingTable from '@/components/pricing-page/pricing-table'
 import { AddOnsTable } from '@/components/pricing-page/add-ons-table'
-import PricingCalculator from '@/components/pricing-page/pricing-calculator'
 import { FloatingCalculatorButton } from '@/components/pricing-page/floating-calculator-button'
+import PricingCalculator from '@/components/pricing-page/pricing-calculator'
 
 export default function PricingPage() {
   return (
     <div>
       <PricingTable />
       <AddOnsTable />
-      <PricingCalculator />
       <FloatingCalculatorButton />
     </div>
   )
